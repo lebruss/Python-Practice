96c9ea950baf4bf0e0447793c5590575cf1959a0
Caleb James Russell <69431631+lebruss@users.noreply.github.com>
2022-02-02 16:58:01 +0200

    Add files via upload
    Next I want to put spacing in the menu for beautification

--- Python_Practice.py
+++ Python_Practice.py
@@ -3,9 +3,10 @@
 
 import random
 
-characterNames = ['Thomas', 'Richard', 'Patrick', 'Robert', 'Seth', 'Parker', 'Sepp', 'Arvo', 'Kreeta', 'Hans']
+characterNames = ['Thomas', 'Richard', 'Patrick', 'Robert', 'Seth', 'Parker', 'Sepp', 'Arvo', 'Kreeta', 'Hans', 'Greeta', 'Peeter', 'Pierre-Luigi', 'Khan']
+familyNames = ['Hansson', 'Schmitt', 'de Groot', 'Jones', 'Blatter', 'Kivisaar']
 party = []
-
+partySize = random.randint(2,5)
 
 class Character:
     isPlayer = 0
@@ -19,19 +20,29 @@ player.age = 24
 #player.name = str(input("What is your name?"))
 #player.age = input("What is your age?")
 player.id = len(party) + 1
+player.familyName = random.choice(familyNames)
 party.append(player)
 
-#Make NPC
-newCharacter = Character()
-newCharacter.name = random.choice(characterNames)
-newCharacter.age = random.randint(15,62)
-newCharacter.id = len(party) + 1
-party.append(newCharacter)
+#debug partySize
+#print("Party size: " + str(partySize))
+
+#Make NPCs
+while len(party) < partySize:
+    newCharacter = Character()
+    newCharacter.name = random.choice(characterNames)
+    newCharacter.familyName = random.choice(familyNames)
+    newCharacter.age = random.randint(15,62)
+    newCharacter.id = len(party) + 1
+    party.append(newCharacter)
 
 #Party roster
 print("ROSTER:")
-
 print("ID   NAME    AGE     EXP")
 print(". . . . .")
 for i in party:
-    print (i.id, i.name, i.age, i.exp)
+    print (i.id, i.name, i.familyName, i.age, i.exp)
+
+
+#press any key to exit
+print("\n Press any key to exit")
+input()
