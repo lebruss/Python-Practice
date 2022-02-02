cbe6e0c6eaf6cdd742e4a6c9d6d03451c7842467
Caleb James Russell <69431631+lebruss@users.noreply.github.com>
2022-02-02 16:22:05 +0200

    Add files via upload
    Added party roster menu

--- Python_Practice.py
+++ Python_Practice.py
@@ -1,11 +1,37 @@
 #Caleb James Russell
 #Python practice
 
-class Estonian:
-    country = 'EST'
+import random
 
-Taavi = Estonian()
-print("Taavi's nationality is: " + Taavi.country)
+characterNames = ['Thomas', 'Richard', 'Patrick', 'Robert', 'Seth', 'Parker', 'Sepp', 'Arvo', 'Kreeta', 'Hans']
+party = []
 
-Taavi.age = int(input("What is Taavi's age? "))
-print("Taavi's age is " + str(Taavi.age))
+
+class Character:
+    isPlayer = 0
+    exp = 0
+
+#User
+player = Character()
+player.isPlayer = 1
+player.name = "Leb"
+player.age = 24
+#player.name = str(input("What is your name?"))
+#player.age = input("What is your age?")
+player.id = len(party) + 1
+party.append(player)
+
+#Make NPC
+newCharacter = Character()
+newCharacter.name = random.choice(characterNames)
+newCharacter.age = random.randint(15,62)
+newCharacter.id = len(party) + 1
+party.append(newCharacter)
+
+#Party roster
+print("ROSTER:")
+
+print("ID   NAME    AGE     EXP")
+print(". . . . .")
+for i in party:
+    print (i.id, i.name, i.age, i.exp)
