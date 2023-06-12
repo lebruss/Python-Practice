70fd880bd127d416a1feed47c62a963b9f6092bd
Caleb James Russell <69431631+lebruss@users.noreply.github.com>
2023-06-12 16:27:21 +0400

    Making a text-RPG to stay busy

---
+++ june 12 2.py
@@ -0,0 +1,29 @@
+#caleb james russell
+
+import time, random, os
+
+#constant variables
+#   list of first names
+#   list of last names
+distance_traveled = 0
+
+#ask for user's name
+
+#how many friends are with you?
+#   create array of that size
+#   friend's names are random firstname + random lastname
+
+#user / friends variables
+'''
+    name
+    money
+'''
+
+
+#menu
+#   view party status
+while True:
+    print("You sleep.")
+    time.sleep(1)
+    os.system('cls||clear')
+    time.sleep(1)
